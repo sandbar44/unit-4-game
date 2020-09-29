33c3f0802a27995f1137a57e1b304759fac33551
Sandra <sandbar44x@gmail.com>
2020-09-29 10:58:19 -0400

    Ready to play

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,176 +1,168 @@
 // Psuedo Code
 // 
+// ==================================================
+// GLOBAL VARIABLES
+// ==================================================
+
+// Character objects
+// [X] Name
+// [X] HP
+// [X] Attack Power (base)
+// [X] Counter Attack Power (enemy)
+// [X] Attack Power (updated)
+
+var hero = "";
+var heroID = "";
+var heroStats = {};
+
+var enemy = "";
+var remEnemies = 0;
+var enemyID = "";
+
+var defender = "";
+var defenderID = "";
+var defenderStats = {};
+
+var chars = {
+	char1: {
+		name: "Rey",
+		origHP: 120,
+		origAP: 8,
+		hp: 120,
+		ap: 8,
+		cap: 24
+	},
+	char2: {
+		name: "Boba Fett",
+		origHP: 100,
+		origAP: 10,
+		hp: 100,
+		ap: 10,
+		cap: 5
+	},
+	char3: {
+		name: "Count Dooku",
+		origHP: 150,
+		origAP: 10,
+		hp: 150,
+		ap: 10,
+		cap: 20
+	},
+	char4: {
+		name: "Darth Maul",
+		origHP: 180,
+		origAP: 12,
+		hp: 180,
+		ap: 12,
+		cap: 25
+	}
+}
+
+// ==================================================
+// FUNCTIONS
+// ==================================================
+
+// Start game
+// [X] Move all characters back to Select Char Div
+// [X] Reset character formatting
+// [X] Reset all character stats
+// [X] Starting instructions
+// [X] Disable Attack button
+// [X] Hide Fight description
+// [X] Disable Restart button
+
+function startGame() {
+	// Move all characters back to Select Char Div
+	for (var i = 1; i < 5; i++) {
+		char = $("#char" + [i]);
+		$("#select-char-div").append(char);
+	}
+
+	// Reset character formatting
+	$(".select-char")
+		.removeClass("btn-success btn-primary btn-danger")
+		.removeAttr("disabled style");
+
+	// Reset all character stats
+	hero = "";
+	heroID = "";
+	heroStats = {};
+
+	enemy = "";
+	remEnemies = 0;
+	enemyID = "";
+
+	defender = "";
+	defenderID = "";
+	defenderStats = {};
+
+	for (var i = 1; i < 5; i++) {
+		char = ("char"+[i]);
+		chars[char].hp = chars[char].origHP;
+		chars[char].ap = chars[char].origAP;
+		$("#char"+[i]+"-hp").html(chars[char].origHP);
+	}
+	
+	// Starting instructions
+	$("#instructions").html("Select a character to begin.")
+
+	// Disable Attack button
+	$(".attack").hide();
+
+	// Hide Fight description
+	$("#fight-desc").hide();
+
+	// Disable Restart button
+	$("#restart").hide();
+}
+
+// In-game Function: New round
+// [X] Reset defender stats
+function newRound() {
+	defender = "";
+	defenderID = "";
+	defenderStats = {};
+}
+
+// In-game Functions: Update stats
+// [X] Update Hero HP
+// [X] Update Hero AP
+// [X] Update Defender HP
+
+function updateHeroHP() {
+	heroStats.hp = (heroStats.hp - defenderStats.cap);
+	$(`#${heroID}-hp`).html(heroStats.hp);
+}
+
+function updateHeroAP() {
+	heroStats.ap = (heroStats.ap + heroStats.origAP);
+}
+
+function updateDefHP() {
+	defenderStats.hp = (defenderStats.hp - heroStats.ap);
+	$(`#${defenderID}-hp`).html(defenderStats.hp);
+}
+
+// End Game
+// [X] Hide Attack button
+// [X] Display Restart button
+// [X] Disable Character buttons
+function endGame() {
+	$(".attack").hide();
+	$("#restart").show();
+	$(".select-char").attr("disabled", true);
+}
+
+// ==================================================
+// GAME PLAY
+// ==================================================
 
 $(document).ready(function () {
 
+	// Start Game
+	startGame();
 
-
-	// ==================================================
-	// GLOBAL VARIABLES
-	// ==================================================
-
-	// Character objects
-	// [X] Name
-	// [X] HP
-	// [X] Attack Power (base)
-	// [X] Counter Attack Power (enemy)
-	// [X] Attack Power (updated)
-
-	var hero = "";
-	var heroID = "";
-	var heroStats = {
-		name: "",
-		hp: "",
-		bap: "",
-		ap: ""
-	};
-
-	var enemy = "";
-	var remEnemies = 0;
-	var enemyID = "";
-
-	var defender = "";
-	var defenderID = "";
-	var defenderStats = {
-		name: "",
-		hp: "",
-		cap: ""
-	};
-
-	var chars = {
-		char1: {
-			name: "Rey",
-			hp: 120,
-			ap: 8,
-			counterAP: 24
-		},
-		char2: {
-			name: "Boba Fett",
-			hp: 100,
-			ap: 10,
-			counterAP: 5
-		},
-		char3: {
-			name: "Count Dooku",
-			hp: 150,
-			ap: 10,
-			counterAP: 20
-		},
-		char4: {
-			name: "Darth Maul",
-			hp: 100,
-			ap: 12,
-			counterAP: 25
-		}
-	}
-
-	// ==================================================
-	// FUNCTIONS
-	// ==================================================
-
-	// Start game
-	// [X] Move all characters back to Select Char Div
-	// [X] Reset character formatting
-	// [X] Reset all character stats
-	// [X] Starting instructions
-	// [X] Disable Attack button
-	// [X] Hide Fight description
-	// [X] Disable Restart button
-
-	function startGame() {
-		// Move all characters back to Select Char Div
-		for (var i = 1; i < 5; i++) {
-			char = $("#char" + [i]);
-			$("#select-char-div").append(char)
-		}
-
-		// Reset character formatting
-		$(".select-char")
-			.removeClass("btn-success btn-primary btn-danger")
-			.removeAttr("disabled style")
-
-		// Reset all character stats
-		hero = "";
-		heroID = "";
-		heroStats = {
-			name: "",
-			hp: "",
-			bap: "",
-			ap: ""
-		};
-
-		enemy = "";
-		remEnemies = 0;
-		enemyID = "";
-
-		defender = "";
-		defenderID = "";
-		defenderStats = {
-			name: "",
-			hp: "",
-			cap: ""
-		};
-
-		$("#char1-hp").html(chars.char1.hp);
-		$("#char2-hp").html(chars.char2.hp);
-		$("#char3-hp").html(chars.char3.hp);
-		$("#char4-hp").html(chars.char4.hp);
-
-		// Starting instructions
-		$("#instructions").html("Select a character to begin.")
-
-		// Disable Attack button
-		$(".attack").hide();
-
-		// Hide Fight description
-		$("#fight-desc").hide();
-
-		// Disable Restart button
-		$("#restart").hide();
-	}
-
-	// New round
-	// [X] Reset defender stats
-	function newRound() {
-		defender = "";
-		defenderID = "";
-		defenderStats = {};
-		console.log(defenderStats)
-	}
-
-	// Update stats
-	// [X] Update Hero HP
-	// [X] Update Hero AP
-	// [X] Update Defender HP
-
-	function updateHeroHP() {
-		heroStats.hp = (heroStats.hp - defenderStats.cap);
-		$("#" + heroID + "-hp").html(heroStats.hp);
-		console.log(heroStats.hp);
-	}
-
-	function updateHeroAP() {
-		heroStats.ap = (heroStats.ap + heroStats.bap)
-	}
-
-	function updateDefHP() {
-		console.log(heroStats.hp);
-		defenderStats.hp = (defenderStats.hp - heroStats.ap);
-		$("#" + defenderID + "-hp").html(defenderStats.hp);
-		console.log(defenderStats)
-	}
-
-	// End Game
-	// [X] Hide Attack button
-	// [X] Display Restart button
-	// [X] Disable Character buttons
-	function endGame() {
-		$(".attack").hide();
-		$("#restart").show();
-		$(".select-char").attr("disabled", true);
-	}
-
-	// Click a character
+	// Gameplay: Click a character
 	// [X] Move clicked character to Your Character section
 	// [X] Move remaining characters to Enemies Available to Attack
 	// [X] Update instructions
@@ -183,7 +175,6 @@ $(document).ready(function () {
 			hero.addClass("btn-success").attr("disabled", true);
 			$("#your-char").append($(hero));
 			heroID = $(hero).attr("id");
-			console.log("heroID = " + heroID);
 
 			// Move remaining characters to Enemies Available to Attack
 			for (var i = 1; i < 5; i++) {
@@ -195,32 +186,16 @@ $(document).ready(function () {
 					remEnemies++;
 				}
 			}
-			console.log(remEnemies)
 
 			// Update instructions
 			$("#instructions").html("Select an enemy to fight!");
 
 			// Set Hero stats
-			if (heroID === "char1") {
-				heroStats.hp = chars.char1.hp;
-				heroStats.bap = chars.char1.ap;
-				heroStats.ap = heroStats.bap;
-			}
-			else if (heroID === "char2") {
-				heroStats.hp = chars.char2.hp;
-				heroStats.bap = chars.char2.ap;
-				heroStats.ap = heroStats.bap;
-			}
-			else if (heroID === "char3") {
-				heroStats.hp = chars.char3.hp;
-				heroStats.bap = chars.char3.ap;
-				heroStats.ap = heroStats.bap;
-			}
-			else if (heroID === "char4") {
-				heroStats.hp = chars.char4.hp;
-				heroStats.bap = chars.char4.ap;
-				heroStats.ap = heroStats.bap;
-			}
+			heroStats = chars[heroID];
+
+			console.log(`heroID = ${heroID} | ${heroStats.name}`);
+			console.log(heroStats);
+			console.log(`Remaining enemies = ${remEnemies}`);
 		}
 	});
 
@@ -237,7 +212,6 @@ $(document).ready(function () {
 			defender.addClass("btn-danger").attr("disabled", true);
 			$("#defender").append($(defender));
 			defenderID = $(defender).attr("id");
-			console.log("defenderID = " + defenderID);
 			$(".attack").show();
 		}
 
@@ -248,30 +222,10 @@ $(document).ready(function () {
 		$("#instructions").html("Attack!");
 
 		// Set Defender stats
-		if (defenderID === "char1") {
-			defenderStats.name = chars.char1.name;
-			defenderStats.hp = chars.char1.hp;
-			defenderStats.cap = chars.char1.counterAP;
-			console.log(defenderStats)
-		}
-		else if (defenderID === "char2") {
-			defenderStats.name = chars.char2.name;
-			defenderStats.hp = chars.char2.hp;
-			defenderStats.cap = chars.char2.counterAP;
-			console.log(defenderStats)
-		}
-		else if (defenderID === "char3") {
-			defenderStats.name = chars.char3.name;
-			defenderStats.hp = chars.char3.hp;
-			defenderStats.cap = chars.char3.counterAP;
-			console.log(defenderStats)
-		}
-		else if (defenderID === "char4") {
-			defenderStats.name = chars.char4.name;
-			defenderStats.hp = chars.char4.hp;
-			defenderStats.cap = chars.char4.counterAP;
-			console.log(defenderStats)
-		}
+		defenderStats = chars[defenderID]
+
+		console.log(`defenderID = ${defenderID} | ${defenderStats.name}`);
+		console.log(defenderStats);
 
 	});
 
@@ -328,7 +282,7 @@ $(document).ready(function () {
 
 				// Decrease Remaining Enemies count
 				remEnemies--;
-				console.log(remEnemies);
+				console.log("Remaining enemies = " + remEnemies)
 
 				// If Enemies Available = 0: (WIN GAME)
 				if (remEnemies === 0) {
@@ -380,6 +334,12 @@ $(document).ready(function () {
 				}
 
 			}
+
+			console.log("Hero");
+			console.log(heroStats);
+			console.log("Defender");
+			console.log(defenderStats);
+
 		}
 	})
 
@@ -389,7 +349,4 @@ $(document).ready(function () {
 		startGame();
 	})
 
-	// 
-	// Start Game
-	startGame();
 })
